9dc054a202d7d3faabeb76d084c4eeda517ca842
emcb-ekanayaka <ekanayakabdr@gmail.com>
2025-09-19 17:53:43 +0530

    SpringBoot Template

--- src/main/java/com/bit/backend/repositories/StylistTaskManagementRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bit.backend.repositories;
-
-import com.bit.backend.entities.StylistTaskManagementEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StylistTaskManagementRepository extends JpaRepository<StylistTaskManagementEntity, Long> {
-}
