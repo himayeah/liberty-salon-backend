5e556cc728a3c26eca0272a5a63d25ce2f164797
himayeah <nirmanihimaya@gmail.com>
2025-12-07 19:44:58 +0530

    Added Getters, Setters annotations

--- src/main/java/com/bit/backend/entities/InventoryEntity.java
+++ src/main/java/com/bit/backend/entities/InventoryEntity.java
@@ -1,10 +1,19 @@
 package com.bit.backend.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+
 @Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
 @Table(name = "inventory")
 public class InventoryEntity {
 
@@ -36,90 +45,5 @@ public class InventoryEntity {
     @Column(name = "product_status")
     private String productStatus;
 
-    public InventoryEntity() {
-    }
 
-    public InventoryEntity(Long id, String productName, String productCategory, String supplier, int productQty, double purchasePrice, double sellingPrice, LocalDate expiryDate, String productStatus) {
-        this.id = id;
-        this.productName = productName;
-        this.productCategory = productCategory;
-        this.supplier = supplier;
-        this.productQty = productQty;
-        this.purchasePrice = purchasePrice;
-        this.sellingPrice = sellingPrice;
-        this.expiryDate = expiryDate;
-        this.productStatus = productStatus;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public String getProductCategory() {
-        return productCategory;
-    }
-
-    public void setProductCategory(String productCategory) {
-        this.productCategory = productCategory;
-    }
-
-    public String getSupplier() {
-        return supplier;
-    }
-
-    public void setSupplier(String supplier) {
-        this.supplier = supplier;
-    }
-
-    public int getProductQty() {
-        return productQty;
-    }
-
-    public void setProductQty(int productQty) {
-        this.productQty = productQty;
-    }
-
-    public double getPurchasePrice() {
-        return purchasePrice;
-    }
-
-    public void setPurchasePrice(double purchasePrice) {
-        this.purchasePrice = purchasePrice;
-    }
-
-    public double getSellingPrice() {
-        return sellingPrice;
-    }
-
-    public void setSellingPrice(double sellingPrice) {
-        this.sellingPrice = sellingPrice;
-    }
-
-    public LocalDate getExpiryDate() {
-        return expiryDate;
-    }
-
-    public void setExpiryDate(LocalDate expiryDate) {
-        this.expiryDate = expiryDate;
-    }
-
-    public String getProductStatus() {
-        return productStatus;
-    }
-
-    public void setProductStatus(String productStatus) {
-        this.productStatus = productStatus;
-    }
 }
